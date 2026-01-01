d62e0427fe0505e2157f859c371b2b296f4eb0b4
yunjoochoi <hnclovera@gmail.com>
2026-01-02 08:35:10 +0900

    vv:

--- run-dev.sh
+++ run-dev.sh
@@ -13,10 +13,12 @@ mkdir -p /home/shaush/projects/docling-serve/docling-models-exports
 docker run --rm \
   --name docling-hack-container \
   -p 5001:5001 \
+  -e DOCLING_SERVE_ENG_KIND=local \
   -e DOCLING_SERVE_LAYOUT_BATCH_SIZE=64 \
   -e DOCLING_SERVE_TABLE_BATCH_SIZE=64 \
   -e PYTHONDONTWRITEBYTECODE=1 \
   -v /home/shaush/projects/docling-serve/docling-models-exports:/opt/app-root/src/.cache/huggingface/hub:z \
   -v /home/shaush/projects/docling-serve/docling-models-exports:/opt/app-root/src/.cache/docling/models:z \
   -v /home/shaush/projects/docling-serve/docling_serve:/opt/app-root/src/docling_serve:z \
+  -v /home/shaush/projects/docling-jobkit/docling_jobkit:/opt/app-root/lib/python3.12/site-packages/docling_jobkit:z \
   docling-hack
